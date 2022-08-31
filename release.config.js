66a7b4db006ef3d01fabe73a58da0d9bb9d28307
John Wright <jrite@protonmail.com>
2022-08-31 22:17:11 +0100

    ci: simpler release scripts

---
+++ release.config.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+/**
+ * @type {import('semantic-release').Options}
+ */
+const config = {
+  branches: ['master'],
+  extends: ['semantic-release-monorepo'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/exec',
+      {
+        verifyConditionsCmd: 'yarn npm whoami --publish',
+        prepareCmd:
+          "yarn version ${nextRelease.version} && echo '::set-output name=version::${nextRelease.version}'",
+        publishCmd: 'yarn npm publish --access public',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'runtyping.schema.json',
+      },
+    ],
+  ],
+}
+
+module.exports = config
