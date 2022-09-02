8ac76ee0be0efacb7924889d73e258f5cb113b33
John Wright <jrite@protonmail.com>
2022-09-02 10:32:47 +0100

    feat: add async targeting
    Targeting predicates could come from anywhere, including somewhere which
    requires an asynchronous operation. Therefore it's seems sensible to
    allow async targeting predicates.
    
    BREAKING CHANGE: All payload API endpoints now return a promise.

--- packages/api/src/types.ts
+++ packages/api/src/types.ts
@@ -3,3 +3,5 @@ import * as rt from 'runtypes'
 export type StaticRecord<R extends Record<string, rt.Runtype>> = {
   [K in keyof R]: rt.Static<R[K]>
 }
+
+export type MaybePromise<T> = T | Promise<T>
