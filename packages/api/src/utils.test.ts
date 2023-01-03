8d4dcf45ab53b5cf0a00b03fe8d36da4f29f2aea
John Wright <jrite@protonmail.com>
2023-01-03 09:35:21 +0000

    test: api utils

---
+++ packages/api/src/utils.test.ts
@@ -0,0 +1,49 @@
+import { arrayInit, arrayLast, objectEveryAsync, objectMap } from './util'
+
+test('objectMap', () => {
+  expect(
+    objectMap(
+      {
+        foo: 123,
+        bar: 321,
+      },
+      (v) => v + 1
+    )
+  ).toEqual({
+    foo: 124,
+    bar: 322,
+  })
+})
+
+test('objectEveryAsync', async () => {
+  expect(
+    await objectEveryAsync(
+      {
+        foo: 1,
+        bar: 2,
+        goo: 3,
+        car: 4,
+      },
+      (_key, value) => Promise.resolve(value).then((v) => v > 2)
+    )
+  ).toBe(false)
+  expect(
+    await objectEveryAsync(
+      {
+        foo: 1,
+        bar: 2,
+        goo: 3,
+        car: 4,
+      },
+      (_key, value) => Promise.resolve(value).then((v) => v > 0)
+    )
+  ).toBe(true)
+})
+
+test('arrayInit', () => {
+  expect(arrayInit([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4])
+})
+
+test('arrayLast', () => {
+  expect(arrayLast([1, 2, 3, 4, 5])).toBe(5)
+})
