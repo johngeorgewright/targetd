7a9d3456b756d2a6fa2d519e1baeb01024d0db89
John Wright <jrite@protonmail.com>
2023-01-04 15:10:35 +0000

    refactor: node callback as last parameter

--- packages/fs/src/watch.ts
+++ packages/fs/src/watch.ts
@@ -3,6 +3,15 @@ import { debounce } from 'lodash'
 import { Options as WatchTreeOptions, unwatchTree, watchTree } from 'watch'
 import { load, pathIsLoadable } from './load'
 
+export type OnLoad<
+  DataValidators extends z.ZodRawShape,
+  TargetingValidators extends z.ZodRawShape,
+  QueryValidators extends z.ZodRawShape
+> = (
+  error: Error | null,
+  data: Data<DataValidators, TargetingValidators, QueryValidators>
+) => any
+
 export function watch<
   DataValidators extends z.ZodRawShape,
   TargetingValidators extends z.ZodRawShape,
@@ -10,12 +19,39 @@ export function watch<
 >(
   data: Data<DataValidators, TargetingValidators, QueryValidators>,
   dir: string,
-  onLoad: (
-    error: Error | null,
-    data: Data<DataValidators, TargetingValidators, QueryValidators>
-  ) => any,
-  options: WatchTreeOptions = {}
+  options: WatchTreeOptions,
+  onLoad: OnLoad<DataValidators, TargetingValidators, QueryValidators>
+): () => void
+
+export function watch<
+  DataValidators extends z.ZodRawShape,
+  TargetingValidators extends z.ZodRawShape,
+  QueryValidators extends z.ZodRawShape
+>(
+  data: Data<DataValidators, TargetingValidators, QueryValidators>,
+  dir: string,
+  onLoad: OnLoad<DataValidators, TargetingValidators, QueryValidators>
+): () => void
+
+export function watch<
+  DataValidators extends z.ZodRawShape,
+  TargetingValidators extends z.ZodRawShape,
+  QueryValidators extends z.ZodRawShape
+>(
+  data: Data<DataValidators, TargetingValidators, QueryValidators>,
+  dir: string,
+  optionsOrOnLoad:
+    | WatchTreeOptions
+    | OnLoad<DataValidators, TargetingValidators, QueryValidators>,
+  onLoadParam?: OnLoad<DataValidators, TargetingValidators, QueryValidators>
 ) {
+  const options = onLoadParam ? optionsOrOnLoad : {}
+  const onLoad = (onLoadParam || optionsOrOnLoad) as OnLoad<
+    DataValidators,
+    TargetingValidators,
+    QueryValidators
+  >
+
   watchTree(
     dir,
     {
