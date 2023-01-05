a3fb22b234d3b7dceea80646ba40344585ebcba5
John Wright <jrite@protonmail.com>
2023-01-05 11:42:53 +0000

    feat: 404 for unknown data properties

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,12 +3,19 @@ import cors from 'cors'
 import express from 'express'
 import queryTypes from 'query-types'
 import { errorHandler } from './middleware/error'
+import { StatusError } from './StatusError'
 
 export function createServer(data: Data<any, any, any>) {
   return express()
     .use(cors())
 
     .get('/:name', queryTypes.middleware(), async (req, res, next) => {
+      if (!(req.params.name in data.dataValidators)) {
+        return next(
+          new StatusError(404, `Unknown data property ${req.params.name}`)
+        )
+      }
+
       let payload: any
       try {
         payload = await data.getPayload(req.params.name, req.query)
