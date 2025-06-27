b67dcc844d917a9ba2f4e6d95f54cb056999fd16
John Wright <jrite@protonmail.com>
2025-06-27 10:09:54 +0100

    fix(explode): typings

--- packages/explode/src/explode.ts
+++ packages/explode/src/explode.ts
@@ -4,6 +4,6 @@ import type { Explode } from './types.ts'
 export function explode<
   T extends Record<string, unknown>,
   PathSeparator extends string,
->(x: T, pathSeparator: string): Explode<T, PathSeparator> {
+>(x: T, pathSeparator: PathSeparator): Explode<T, PathSeparator> {
   return unflatten(x, { delimiter: pathSeparator })
 }
