b266736d2a2a40afdd79b4d6043f24b614802f49
John Wright <jrite@protonmail.com>
2025-08-19 09:05:06 +0100

    chore: slow types

--- packages/date-range/src/index.ts
+++ packages/date-range/src/index.ts
@@ -1,40 +1,78 @@
-import { createTargetingDescriptor } from '@targetd/api'
-import { array, partial, regex, strictObject, string, union } from 'zod/mini'
-import type { output } from 'zod/v4/core'
+import {
+  createTargetingDescriptor,
+  type TargetingDescriptor,
+} from '@targetd/api'
+import {
+  array,
+  partial,
+  regex,
+  strictObject,
+  string,
+  union,
+  type ZodMiniArray,
+  type ZodMiniObject,
+  type ZodMiniOptional,
+  type ZodMiniString,
+  type ZodMiniUnion,
+} from 'zod/mini'
+import type { $strict, output } from 'zod/v4/core'
 
-const ISODateTime = string().check(
+type ISODateTimeParser = ZodMiniString<string>
+
+const isoDateTimeParser: ISODateTimeParser = string().check(
   regex(
     /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])(T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?)?$/,
     'Must represent an ISO date',
   ),
 )
 
-const DateRange = partial(strictObject({
-  end: ISODateTime,
-  start: ISODateTime,
+type DateRangeParser = ZodMiniObject<{
+  end: ZodMiniOptional<ISODateTimeParser>
+  start: ZodMiniOptional<ISODateTimeParser>
+}, $strict>
+
+const dateRangeParser: DateRangeParser = partial(strictObject({
+  end: isoDateTimeParser,
+  start: isoDateTimeParser,
 }))
 
-type DateRange = output<typeof DateRange>
+type DateRange = output<typeof dateRangeParser>
 
-const dateRangeTargeting = createTargetingDescriptor({
+type TargetingParser = ZodMiniUnion<
+  [typeof dateRangeParser, ZodMiniArray<typeof dateRangeParser>]
+>
+
+const targetingParser: TargetingParser = union([
+  dateRangeParser,
+  array(dateRangeParser),
+])
+
+const dateRangeTargeting: TargetingDescriptor<
+  TargetingParser,
+  typeof dateRangeParser,
+  {
+    end?: string
+    start?: string
+  }
+> = createTargetingDescriptor({
   predicate: (q) => (t) =>
     Array.isArray(t) ? dateRangesPredicate(t, q) : dateRangePredicate(t, q),
-  queryParser: DateRange,
+  queryParser: dateRangeParser,
   requiresQuery: false,
-  targetingParser: union([DateRange, array(DateRange)]),
+  targetingParser,
 })
 
 export default dateRangeTargeting
 
-function dateRangePredicate(t: DateRange, q?: DateRange) {
+function dateRangePredicate(t: DateRange, q?: DateRange): boolean {
   return q?.start || q?.end ? queryDateRange(t, q) : queryDateRangeAgainstNow(t)
 }
 
-function dateRangesPredicate(ts: DateRange[], q?: DateRange) {
+function dateRangesPredicate(ts: DateRange[], q?: DateRange): boolean {
   return ts.length === 0 || ts.some((t) => dateRangePredicate(t, q))
 }
 
-function queryDateRange(t: DateRange, q: DateRange) {
+function queryDateRange(t: DateRange, q: DateRange): boolean {
   const qStart = q.start ? new Date(q.start).getTime() : 0
   const tStart = t.start ? new Date(t.start).getTime() : 0
   const qEnd = q.end ? new Date(q.end).getTime() : Infinity
--- packages/json-schema/src/index.ts
+++ packages/json-schema/src/index.ts
@@ -1,7 +1,10 @@
 import { DataItemParser, DataItemsParser, type DT } from '@targetd/api'
 import { extend, optional, string, toJSONSchema } from 'zod/mini'
+import type { JSONSchema } from 'zod/v4/core'
 
-export function dataJSONSchemas<D extends DT.Any>(data: D) {
+export function dataJSONSchemas<D extends DT.Any>(
+  data: D,
+): JSONSchema.BaseSchema {
   return toJSONSchema(
     extend(
       DataItemsParser(
@@ -20,7 +23,7 @@ export function dataJSONSchemas<D extends DT.Any>(data: D) {
 export function dataJSONSchema<D extends DT.Any>(
   data: D,
   name: keyof DT.PayloadParsers<D>,
-) {
+): JSONSchema.BaseSchema {
   return toJSONSchema(
     extend(
       DataItemParser(
