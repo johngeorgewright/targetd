3b357c3dc6a741a28c28518ad73b77321675b6c2
John Wright <jrite@protonmail.com>
2025-08-20 14:16:29 +0100

    Revert "feat: default queryparser"
    This reverts commit 50d12a66fd1b6dcf97a18a1b1fe53d0dcd533b18.

--- packages/api/src/createTargetingDescriptor.ts
+++ packages/api/src/createTargetingDescriptor.ts
@@ -1,18 +1,12 @@
 import type { $ZodType } from 'zod/v4/core'
 import type TargetingDescriptor from './parsers/TargetingDescriptor.ts'
-import type { TargetingDescriptorInput } from './parsers/TargetingDescriptor.ts'
-import { unknown, type ZodMiniUnknown } from 'zod/mini'
 
 export default function createTargetingDescriptor<
   TV extends $ZodType,
-  QV extends $ZodType = ZodMiniUnknown,
+  QV extends $ZodType,
   Query extends Record<string, any> = {},
 >(
-  targetingDescriptor: TargetingDescriptorInput<TV, QV, Query>,
+  targetingDescriptor: TargetingDescriptor<TV, QV, Query>,
 ): TargetingDescriptor<TV, QV, Query> {
-  return {
-    requiresQuery: true,
-    queryParser: (targetingDescriptor.queryParser ?? unknown()) as QV,
-    ...targetingDescriptor,
-  }
+  return { requiresQuery: true, ...targetingDescriptor }
 }
