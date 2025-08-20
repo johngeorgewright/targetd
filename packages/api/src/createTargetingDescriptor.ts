50d12a66fd1b6dcf97a18a1b1fe53d0dcd533b18
John Wright <jrite@protonmail.com>
2025-08-20 13:19:11 +0100

    feat: default queryparser

--- packages/api/src/createTargetingDescriptor.ts
+++ packages/api/src/createTargetingDescriptor.ts
@@ -1,12 +1,18 @@
 import type { $ZodType } from 'zod/v4/core'
 import type TargetingDescriptor from './parsers/TargetingDescriptor.ts'
+import type { TargetingDescriptorInput } from './parsers/TargetingDescriptor.ts'
+import { unknown, type ZodMiniUnknown } from 'zod/mini'
 
 export default function createTargetingDescriptor<
   TV extends $ZodType,
-  QV extends $ZodType,
+  QV extends $ZodType = ZodMiniUnknown,
   Query extends Record<string, any> = {},
 >(
-  targetingDescriptor: TargetingDescriptor<TV, QV, Query>,
+  targetingDescriptor: TargetingDescriptorInput<TV, QV, Query>,
 ): TargetingDescriptor<TV, QV, Query> {
-  return { requiresQuery: true, ...targetingDescriptor }
+  return {
+    requiresQuery: true,
+    queryParser: (targetingDescriptor.queryParser ?? unknown()) as QV,
+    ...targetingDescriptor,
+  }
 }
--- packages/api/src/parsers/TargetingDescriptor.ts
+++ packages/api/src/parsers/TargetingDescriptor.ts
@@ -1,19 +1,53 @@
 import type { $ZodType } from 'zod/v4/core'
 import type TargetingPredicate from './TargetingPredicate.ts'
+import type { ZodMiniUnknown } from 'zod/mini'
+
+export interface TargetingDescriptorInput<
+  TV extends $ZodType,
+  QV extends $ZodType = ZodMiniUnknown,
+  Query extends Record<string, any> = {},
+> {
+  /**
+   * The assertion that a query can match the targeting.
+   *
+   * @example
+   * ```
+   * (query) => (targeting) => targeting === query
+   * ```
+   */
+  predicate: TargetingPredicate<QV, TV, Query>
+
+  /**
+   * Validate a query parameter before it reaches the predicate.
+   *
+   * @default ZodMiniUnknown
+   */
+  queryParser?: QV
+
+  /**
+   * Whether the query property has to exist for the predicate
+   * to even run.
+   *
+   * @default true
+   */
+  requiresQuery?: boolean
+
+  /**
+   * Validate targeting before it's stored.
+   */
+  targetingParser: TV
+}
 
 /**
  * Instructions on a targeting field.
  */
-export default interface TargetingDescriptor<
+type TargetingDescriptor<
   TV extends $ZodType,
   QV extends $ZodType,
-  Query extends Record<string, any> = {},
-> {
-  predicate: TargetingPredicate<QV, TV, Query>
-  queryParser: QV
-  requiresQuery?: boolean
-  targetingParser: TV
-}
+  Query extends Record<string, any>,
+> = Required<TargetingDescriptorInput<TV, QV, Query>>
+
+export default TargetingDescriptor
 
 export function isTargetingDescriptor<
   TV extends $ZodType,
