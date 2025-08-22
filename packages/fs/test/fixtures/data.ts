0a6f41e9215a382954e07d31c01c5e2486897df7
John Wright <jrite@protonmail.com>
2025-08-22 17:32:09 +0100

    fix(fs): allow other properties

--- packages/fs/test/fixtures/data.ts
+++ packages/fs/test/fixtures/data.ts
@@ -1,8 +1,9 @@
-import { Data } from '@targetd/api'
-import { z } from 'zod'
+import { Data, targetIncludes } from '@targetd/api'
+import { string, z } from 'zod'
 
 export const data = await Data.create()
   .usePayload({
     foo: z.string(),
     b: z.string(),
   })
+  .useTargeting({ channel: targetIncludes(string()) })
