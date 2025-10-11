0323e34bbb7c228ba9e1024cc66b31897fd3e642
John Wright <jrite@protonmail.com>
2025-10-11 10:07:27 +0100

    refactor(server): typing middleware

--- packages/server/src/middleware/castQueryArrayProps.ts
+++ packages/server/src/middleware/castQueryArrayProps.ts
@@ -2,12 +2,18 @@ import type { Data } from '@targetd/api'
 import type { RequestHandler } from 'express'
 import type { $ZodShape } from 'zod/v4/core'
 import type { MaybePromise } from '../types.ts'
+import type { ParsedQs } from './castQueryProp.ts'
 
 export function castQueryArrayProps<
   PayloadParsers extends $ZodShape,
   TargetingParsers extends $ZodShape,
   QueryParsers extends $ZodShape,
   FallThroughTargetingParsers extends $ZodShape,
+  P extends Record<string, string>,
+  ResBody,
+  ReqBody,
+  ReqQuery,
+  Locals extends { query: ParsedQs },
 >(
   getData: () => MaybePromise<
     Data<
@@ -17,18 +23,17 @@ export function castQueryArrayProps<
       FallThroughTargetingParsers
     >
   >,
-): RequestHandler {
-  return async (req, res, next) => {
+): RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals> {
+  return async (_req, res, next) => {
     const { queryParsers } = await getData()
 
-    for (const [key, value] of Object.entries(req.query)) {
+    for (const [key, value] of Object.entries(res.locals.query)) {
       if (
         key in queryParsers &&
         queryParsers[key]._zod.def.type === 'array' &&
         value !== undefined &&
         !Array.isArray(value)
       ) {
-        res.locals.query ?? {}
         res.locals.query[key] = [value]
       }
     }
