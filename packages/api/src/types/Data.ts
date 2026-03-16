e3d135f37acfc1b0660c261f2b253caa3b9ec100
John Wright <jrite@protonmail.com>
2026-03-16 11:05:44 +0000

    refactor: reuse type helpers

--- packages/api/src/types/Data.ts
+++ packages/api/src/types/Data.ts
@@ -45,30 +45,24 @@ export type $<D extends Any> = D extends Data<infer $> ? $ : never
 /**
  * Get the PayloadParsers from a Data type
  */
-export type PayloadParsers<D extends Any> = D extends Data<infer $>
-  ? $['PayloadParsers']
-  : never
+export type PayloadParsers<D extends Any> = $<D>['PayloadParsers']
 
 /**
  * Get the TargetingParsers from a Data type
  */
-export type TargetingParsers<D extends Any> = D extends Data<infer $>
-  ? $['TargetingParsers']
-  : never
+export type TargetingParsers<D extends Any> = $<D>['TargetingParsers']
 
 /**
  * Get the QueryParsers from a Data type
  */
-export type QueryParsers<D extends Any> = D extends Data<infer $>
-  ? $['QueryParsers']
-  : never
+export type QueryParsers<D extends Any> = $<D>['QueryParsers']
 
 /**
  * Get the FallThroughTargetingParsers from a Data type
  */
-export type FallThroughTargetingParsers<D extends Any> = D extends Data<infer $>
-  ? $['FallThroughTargetingParsers']
-  : never
+export type FallThroughTargetingParsers<D extends Any> = $<
+  D
+>['FallThroughTargetingParsers']
 
 /**
  * Data shape that can be inserted into a Data instance.
